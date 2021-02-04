3ff7250946db3be0d57c12d600b7bde62353b2f0
huyvmdn <huyvm.dn@gmail.com>
2021-02-04 10:57:17 +0700

    lan5

---
+++ module_2/taiLieu/NewClass.java
@@ -0,0 +1,19 @@
+public class NewClass {
+    int x;
+
+    void display() {
+        System.out.println("x la:"+x);
+    }
+
+    public static void main(String[] args) {
+        NewClass ob = new NewClass();
+
+        System.out.println(ob.x);
+
+    }
+}
+public class Chay {
+    public static void main(String[] args) {
+
+    }
+}
